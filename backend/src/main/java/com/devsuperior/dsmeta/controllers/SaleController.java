f75a72818409aa0ae73b53ab21beafc144c4802f
jiuliogusmao <jiuliogusmao@gmail.com>
2022-11-18 11:42:16 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,36 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.devsuperior.dsmeta.controllers;
 
-import com.devsuperior.dsmeta.entities.Sale;
-import com.devsuperior.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author jiulio
- */
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
+
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
-    
-    @Autowired
-    private SaleService service;
-    
-    @GetMapping
-    public Page<Sale> findSales(
-            @RequestParam(value="minDate", defaultValue = "") String minDate, 
-            @RequestParam(value="maxDate", defaultValue = "") String maxDate, 
-            Pageable pageable){
-        return service.findSales(minDate, maxDate, pageable);
-    }
-    
+
+	@Autowired
+	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
+	
+	@GetMapping
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
 }
